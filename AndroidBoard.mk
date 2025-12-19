90049e04e19cc309dae1e411ecbebb583f37422a
Bharat Pawar <c_bpawar@qti.qualcomm.com>
2025-12-19 17:57:56 +0530

    lahaina612: Update sectools path
    Updated sectools removing proprietary path.
    
    Change-Id: Ie44050d06038558ccd75dd3e89964d4d7b782cf3

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -23,7 +23,7 @@ else
 TARGET_EMMC_BOOTLOADER := $(TARGET_BOARD_UNSIGNED_ABL_DIR)/unsigned_abl.elf
 SIGN_ABL := $(PRODUCT_OUT)/abl.elf
 
-SECIMAGE_BASE := vendor/qcom/proprietary/sectools
+SECIMAGE_BASE := $(QCPATH)/sectools
 SECTOOLS_SECURITY_PROFILE := $(SECIMAGE_BASE)/config/integration/secimage_eccv3.xml
 SIGN_ID := abl
 INSTALL_FILE_NAME := abl.elf
